95451ca701e3ee18e407fcdaf688a0f7ec153bc6
Monika Mikec <monikamikec03@gmail.com>
2024-02-29 11:26:02 +0100

    Route Model Binding Form Requests, Mass Assignment

--- routes/web.php
+++ routes/web.php
@@ -40,13 +40,6 @@ Route::get('/tasks/{task}', function (Task $task) {
 })->name('tasks.show');
 
 Route::post('/tasks', function (TaskRequest $request) {
-    // $data = $request->validated();
-
-    // $task = new Task();
-    // $task->title = $data['title'];
-    // $task->description = $data['description'];
-    // $task->long_description = $data['long_description'];
-    // $task->save();
     $task = Task::create($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
@@ -54,13 +47,6 @@ Route::post('/tasks', function (TaskRequest $request) {
 })->name('tasks.store');
 
 Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
-    // $data = $request->validated();
-
-    // $task->title = $data['title'];
-    // $task->description = $data['description'];
-    // $task->long_description = $data['long_description'];
-    // $task->save();
-
     $task->update($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
