3daac6ebe652e9b221850911384964d606232379
Monika Mikec <monikamikec03@gmail.com>
2024-02-29 13:52:13 +0100

    Adding Styling with Tailwind CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,14 @@
 
 @section('content')
 
-    <div>
-        <a href="{{ route('tasks.create') }}">Add Task</a>
-    </div>
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}" class="font-medium text-gray-700 underline decoration-pink-500">Add Task</a>
+    </nav>
     <div>
         @forelse($tasks as $task)
             <div>
-                <a href="{{ route('tasks.show', ['task' => $task->id]) }}">
+                <a href="{{ route('tasks.show', ['task' => $task->id]) }}" 
+                    @class(['line-through' => $task->completed])>
                     {{ $task->title }}
                 </a>
             </div>
@@ -20,7 +21,7 @@
         @endforelse
 
         @if($tasks->count())
-            <nav>
+            <nav class="mt-4">
                 {{ $tasks->links() }}
             </nav>
         @endif
